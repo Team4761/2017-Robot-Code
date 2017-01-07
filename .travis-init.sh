c8a5c1f03745ed24a8636770eb13c23d68a6efef
Simon Andrews <simon.andrews212@gmail.com>
2017-01-07 16:50:17 -0500

    Hopefully fix Travis init script by creating some new directory

--- .travis-init.sh
+++ .travis-init.sh
@@ -1,4 +1,4 @@
-mkdir ~/wpilib
+mkdir -p ~/wpilib/user/java/lib
 echo "version=current\nteam-number=4761" > ~/wpilib/wpilib.properties
 wget https://gist.githubusercontent.com/simon-andrews/0127707da461e543cb76/raw/dl_wpilibjars.py
 python3 dl_wpilibjars.py --dest ~/wpilib/java/current --members ant/ant-classloadertask.jar \
