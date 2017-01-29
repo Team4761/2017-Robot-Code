21293ed14f1b8a5178054b45d047fb5a9b84cf8b
Simon Andrews <simon.andrews212@gmail.com>
2017-01-29 15:28:57 -0500

    Drive -> DriveStraight, plus needed changes

--- src/org/robockets/steamworks/drivetrain/DriveStraight.java
+++ src/org/robockets/steamworks/drivetrain/DriveStraight.java
@@ -9,32 +9,27 @@ import org.robockets.steamworks.Robot;
  * @author Jake Backer
  */
 public class DriveStraight extends Command {
-    private int offset;
     private RelativeDirection.YAxis direction;
     private double speed;
 
     /**
      * Setup drive
-     * @param offset How much to offset rotation between -1 and 1
      * @param direction Forwards or Backwards
      * @param speed Speed multiplier
      */
-    public DriveStraight(int offset, RelativeDirection.YAxis direction, double speed) {
-        this.offset = offset;
+    public DriveStraight(RelativeDirection.YAxis direction, double speed) {
         this.direction = direction;
         this.speed = speed;
     }
 
-    protected void initialize() {
-
-    }
+    protected void initialize() {}
 
     protected void execute() {
-        if (direction == RelativeDirection.YAxis.FORWARD) {
-            Robot.drivetrain.driveArcade(0.5*speed, offset*0.5*speed); // 0.5 is the default speed. This is subject to change
-        } else {
-            Robot.drivetrain.driveArcade(-0.5*speed, offset*-0.5*speed);
+        if (direction == RelativeDirection.YAxis.BACKWARD) {
+        	speed = -speed;
+            
         }
+        Robot.drivetrain.driveTank(speed, speed);
     }
 
     protected boolean isFinished() {
