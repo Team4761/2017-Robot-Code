37014f1d27121b63acf1e716d1d903e48e7b4fb9
Brian Shin <brianshin100@yahoo.com>
2017-01-31 22:26:19 -0500

    Add methods for driving along an arc, add javadocs

--- src/org/robockets/steamworks/drivetrain/Drivetrain.java
+++ src/org/robockets/steamworks/drivetrain/Drivetrain.java
@@ -2,6 +2,8 @@ package org.robockets.steamworks.drivetrain;
 
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.command.Subsystem;
+
+import org.robockets.commons.RelativeDirection;
 import org.robockets.steamworks.DummyPIDOutput;
 import org.robockets.steamworks.RobotMap;
 import org.robockets.steamworks.pidsources.EncoderPIDSource;
@@ -63,14 +65,41 @@ public class Drivetrain extends Subsystem {
         disableEncoderPID();
     }
     
+    /**
+     * A method to drive straight for a certain distance
+     * @param distance Distance, in inches
+     */
     public void driveDistance(double distance) {
     	setDistanceInInches(distance);
     	RobotMap.robotDrive.arcadeDrive(leftPodPID.get(), 0);
     }
     
     /**
-     * A basic method to set the setpoint of the robot for a given distance with PID
-     * @param distance Desired distance, in inches
+     * A method to drive along an arc with PID, its usefulness is debatable
+     * @param direction Direction of the turn, left or right
+     * @param chordLength Length of the hypotenuse of the triangle formed by two radii such that they are perpendicular
+     * @param radius Distance between the robot and a point directly below the target
+     * @param speed Speed scalar
+     */
+    public void driveArc(RelativeDirection.XAxis direction, double chordLength, double radius, double speed) {
+    	double CENTERPOINT_TO_WHEEL = 0; // In inches, of course
+    	double arcLengthLeft;
+    	double arcLengthRight;
+    	if(direction == RelativeDirection.XAxis.RIGHT) {
+    		arcLengthLeft = calculateArcLength(chordLength, radius + CENTERPOINT_TO_WHEEL);
+    		arcLengthRight = calculateArcLength(chordLength, radius - CENTERPOINT_TO_WHEEL);
+    	} else {
+    		arcLengthLeft = calculateArcLength(chordLength, radius - CENTERPOINT_TO_WHEEL);
+    		arcLengthRight = calculateArcLength(chordLength, radius + CENTERPOINT_TO_WHEEL);
+    	}
+    	
+    	setDistanceInInches(arcLengthLeft, arcLengthRight);
+    	driveTank(leftPodPID.get() * speed, rightPodPID.get() * speed);
+    }
+    
+    /**
+     * A basic method to set the setpoint of both of the pods for a given distance with PID
+     * @param distance Desired distance for both pods, in inches
      */
     public void setDistanceInInches(double distance) {
     	leftPodPID.setSetpoint(distance); // This is wrong, find encoder ticks per inch and edit the parameter on EncoderPIDSource!
@@ -79,12 +108,33 @@ public class Drivetrain extends Subsystem {
         leftPodPID.enable();
     	rightPodPID.enable();
     }
-
+    
+    /**
+     * A basic method to set the setpoint of each of the pods for a given distance with PID
+     * @param leftDistance Desired distance for the left pod, in inches
+     * @param rightDistance Desired distance for the right pod, in inches
+     */
+    public void setDistanceInInches(double leftDistance, double rightDistance) {
+    	leftPodPID.setSetpoint(leftDistance);
+    	rightPodPID.setSetpoint(rightDistance);
+    	
+    	leftPodPID.enable();
+    	rightPodPID.enable();
+    }
+    
+    /**
+     * Method to turn the robot in place relative to where 0 degrees is
+     * @param angle Desired angle in degrees
+     */
     public void absoluteTurn(double angle) {
         gyroPID.setSetpoint(angle);
         gyroPID.enable();
     }
 
+    /**
+     * A method to turn the robot in place relative to current position
+     * @param angle Desired angle in degrees
+     */
     public void relativeTurn(double angle) {
         double newAngle = gyroPIDSource.pidGet() + angle;
         gyroPID.setSetpoint(newAngle);
@@ -99,6 +149,16 @@ public class Drivetrain extends Subsystem {
     }
     
     /**
+     * Method for calculating arc length
+     * @param chordLength Distance from point A to B on the circumference of a circle
+     * @param radius Radius of the circle
+     * @return Returns the arc length, in inches
+     */
+    public double calculateArcLength(double chordLength, double radius) {
+    	return Math.toDegrees(Math.asin(chordLength / radius/ 2)) * 96;
+    }
+    
+    /**
      * A method to stop the drivetrain
      */
     public void stop() {
@@ -106,8 +166,12 @@ public class Drivetrain extends Subsystem {
         gyroPID.disable();
     }
     
+    /**
+     * A method to disable the encoder PIDs
+     */
     public void disableEncoderPID() {
     	leftPodPID.reset();
+    	rightPodPID.reset();
     }
 
 }
