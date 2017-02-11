ce64abcbf8af3e8b9a280737bd3cc57b018904a7
Jake Backer <jbacker42@gmail.com>
2017-02-11 12:26:38 -0500

    Remove MoveDrivePodWithMP Command, remove comments, remove unused imports

--- src/org/robockets/steamworks/drivetrain/DriveWithMP.java
+++ src/org/robockets/steamworks/drivetrain/DriveWithMP.java
@@ -5,7 +5,6 @@ import org.robockets.steamworks.Robot;
 import org.robockets.steamworks.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Motion profiled driving
@@ -28,31 +27,25 @@ public class DriveWithMP extends Command {
         this.velocity = velocity;
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
     	mp = new LinearSetpointGenerator(targetPosition , velocity, RobotMap.leftEncoder.getDistance());
     	Robot.drivetrain.leftPodPID.enable();
     	Robot.drivetrain.rightPodPID.enable();
     }
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	double setpoint = mp.next();
     	Robot.drivetrain.setDistanceInInches(setpoint);
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
 		return !mp.hasNext();
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     	Robot.drivetrain.disableEncoderPID();
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     	end();
     }
