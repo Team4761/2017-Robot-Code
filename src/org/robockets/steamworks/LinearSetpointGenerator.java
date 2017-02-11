8430c21680ddc5b8ead57391cc2e359238aeb02c
Simon Andrews <simon.andrews212@gmail.com>
2017-02-11 12:45:32 -0500

    Add JavaDocs to LinearSetpoint generator.

--- src/org/robockets/steamworks/LinearSetpointGenerator.java
+++ src/org/robockets/steamworks/LinearSetpointGenerator.java
@@ -4,14 +4,45 @@ import java.util.Iterator;
 import java.util.function.Consumer;
 
 public class LinearSetpointGenerator implements Iterator<Double> {
+	/**
+	 * Final setpoint for whatever it is you're controlling. Unit is whatever
+	 * the unit is for the value returned from
+	 * {@link edu.wpi.first.wpilibj.PIDSource#pidGet()} over seconds.
+	 */
 	private double targetPosition;
+	/**
+	 * How fast you want to go, in units per second.
+	 */
 	private double velocity;
+	/**
+	 * Initial state of the PID source.
+	 */
 	private double initialPosition;
+	/**
+	 * How long is it expected for the robot to follow this path, using
+	 * displacement and velocity.
+	 */
 	private double time;
+	/**
+	 * Number of seconds between each setpoint.
+	 */
 	private final double stepLength = 0.02; //seconds
+	/**
+	 * Number of steps (setpoint changes) it will take to get from the initial
+	 * position to the target position.
+	 */
 	private double stepCount;
+	/**
+	 * How much the setpoint changes every step.
+	 */
 	private double stepHeight;
+	/**
+	 * How far the robot will travel.
+	 */
 	private double totalDisplacement;
+	/**
+	 * Number of times the {@link #next()} method has been called.
+	 */
 	private double getNextCount;
 
 	/**
@@ -43,11 +74,17 @@ public class LinearSetpointGenerator implements Iterator<Double> {
 		
 	}
 
+	/**
+	 * @return Are there any remaining setpoints in the path?
+	 */
 	@Override
 	public boolean hasNext() {
 		return !(getNextCount>=stepCount); // FIXME: This is 100% wrong
 	}
 
+	/**
+	 * @return The next setpoint in the path.
+	 */
 	@Override
 	public Double next() {
 		return initialPosition + (getNextCount++ * stepHeight);
@@ -55,7 +92,7 @@ public class LinearSetpointGenerator implements Iterator<Double> {
 
 	@Override
 	public void remove() {
-		// TODO Auto-generated method stub
+		// TODO Implement this?
 		
 	}
 	
