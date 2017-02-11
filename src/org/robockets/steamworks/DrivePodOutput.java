6418e985cd6659c98f022e3fe582b9c3e0889fee
Jake Backer <jbacker42@gmail.com>
2017-02-11 14:02:31 -0500

    Add javadocs

--- src/org/robockets/steamworks/DrivePodOutput.java
+++ src/org/robockets/steamworks/DrivePodOutput.java
@@ -3,10 +3,18 @@ package org.robockets.steamworks;
 import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.SpeedController;
 
+/**
+ * PIDOutput used for drivepods
+ */
 public class DrivePodOutput implements PIDOutput {
 
 	private SpeedController c1, c2;
-	
+
+	/**
+	 *
+	 * @param c1 Left side speed controller
+	 * @param c2 Right side speed controller
+	 */
 	public DrivePodOutput(SpeedController c1, SpeedController c2) {
 		this.c1 = c1;
 		this.c2 = c2;
